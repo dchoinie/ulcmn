4fd15f1d8d8396c93b8220f1c89f7bb3c62000fa
Dan Choiniere <dchoinie@gmail.com>
2019-07-14 15:25:38 -0500

    Footer responsive sizing
    Added responsive sizing to footer to make vertical on small screen and horizontal on large screens

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -11,35 +11,37 @@ export class footer extends Component {
         className="py-6 text-white flex flex-col lg:flex-row justify-between"
         style={{ backgroundColor: "var(--gray-main)" }}
       >
-        <div className="flex justify-center my-2 lg:block">
+        <div className="flex justify-center my-2 lg:block lg:mx-12 lg:my-0">
           <img src={LCMS} alt="LCMS Logo" style={{ maxWidth: "200px" }} />
         </div>
-        <div className="flex justify-center my-2 lg:block">
-          <ul className="flex-col text-center lg:flex-row">
-            <li className="mx-1">
-              <DropdownAbout />
-            </li>
-            <li className="mx-1">Calendar</li>
-            <li className="mx-1">Map &#38; Shuttle</li>
-            <li className="mx-1">
-              <DropdownSupport />
-            </li>
-            <li className="mx-1">
-              <DropdownResources />
-            </li>
-            <li className="mx-1">Library</li>
-            <li className="mx-1">Directory</li>
-            <li className="mx-1">Contact</li>
-          </ul>
-        </div>
-        <div className="flex justify-center my-2 lg:block">
-          <small
-            className="text-center"
-            style={{ color: "var(--yellow-main)" }}
-          >
-            © {new Date().getFullYear()} · University Lutheran Chapel · 316 10th
-            Ave SE, Minneapolis MN 55414 · 612-331-2747
-          </small>
+        <div className="mx-12">
+          <div className="flex justify-center my-2 lg:block lg:my-0">
+            <ul className="flex-col text-center lg:flex lg:flex-row">
+              <li className="mx-1">
+                <DropdownAbout />
+              </li>
+              <li className="mx-1">Calendar</li>
+              <li className="mx-1">Map &#38; Shuttle</li>
+              <li className="mx-1">
+                <DropdownSupport />
+              </li>
+              <li className="mx-1">
+                <DropdownResources />
+              </li>
+              <li className="mx-1">Library</li>
+              <li className="mx-1">Directory</li>
+              <li className="mx-1">Contact</li>
+            </ul>
+          </div>
+          <div className="flex justify-center my-2 lg:block lg:text-right lg:my-0">
+            <small
+              className="text-center"
+              style={{ color: "var(--yellow-main)" }}
+            >
+              © {new Date().getFullYear()} · University Lutheran Chapel · 316
+              10th Ave SE, Minneapolis MN 55414 · 612-331-2747
+            </small>
+          </div>
         </div>
       </div>
     );
